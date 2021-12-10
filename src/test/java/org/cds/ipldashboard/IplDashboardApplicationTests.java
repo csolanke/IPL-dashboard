74aecac0b9cd724bc5081236fc350a110b8953f5
Chandrakant <csolanke77@gmail.com>
2021-12-09 21:00:17 -0500

    Added initial commits for ipl-dashboard backend service

---
+++ src/test/java/org/cds/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cds.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
