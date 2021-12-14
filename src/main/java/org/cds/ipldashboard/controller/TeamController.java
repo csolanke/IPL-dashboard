9270f19d967a6a9c5c901ee5f0b46dd8e1b5c769
Chandrakant <csolanke77@gmail.com>
2021-12-14 17:50:41 -0500

    Added match backend api

--- src/main/java/org/cds/ipldashboard/controller/TeamController.java
+++ src/main/java/org/cds/ipldashboard/controller/TeamController.java
@@ -1,11 +1,16 @@
 package org.cds.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.cds.ipldashboard.model.Match;
 import org.cds.ipldashboard.model.Team;
 import org.cds.ipldashboard.repository.MatchRepository;
 import org.cds.ipldashboard.repository.TeamRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -27,6 +32,15 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/teams/{teamName}/matches")
+    public List<Match> getMatchesForTem(@PathVariable String teamName, @RequestParam int year)
+    {
+        LocalDate dateStart = LocalDate.of(year, 1, 1);
+        LocalDate  dateEnd = LocalDate.of(year +1 , 1, 1);
+
+        return matchRepository.getMatchesByTeamBetweenDates(teamName, dateStart, dateEnd);
     }
 
 }
