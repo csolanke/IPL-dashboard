1ed2b0350be96e3de0d37685c358e2081d17521d
Chandrakant <csolanke77@gmail.com>
2021-12-15 22:50:25 -0500

    Added Styling for TeamPage component

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,16 +1,19 @@
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './MatchSmallCard.scss';
 
  export const MatchSmallCard = ({teamName,match}) =>
 {  
     if(!match) return null;
     const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     const otherTeamRoute = `/teams/${otherTeam}`;
+    const isMatchWon = teamName===match.matchWinner;
 
     return (
-        <div className ="MatchSmallCard">
-             <h3>vs 
-                 <Link to ={otherTeamRoute}>{otherTeam} </Link></h3>
-             <p>{match.matchWinner} Won by {match.resultMargin} {match.result}</p>
+        <div className ={isMatchWon ? ' MatchSmallCard won-card':' MatchSmallCard lost-card'}>
+            
+             <span className="vs">vs</span>
+             <h3><Link to ={otherTeamRoute}>{otherTeam} </Link></h3>
+             <p className="match-result">{match.matchWinner} Won by {match.resultMargin} {match.result}</p>
 
         </div>
     );
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,45 +1,63 @@
-import {React, useEffect,useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { useParams } from 'react-router-dom';
+import { PieChart } from 'react-minimal-pie-chart';
+import './TeamPage.scss';
 
-export const TeamPage =()=> {
+export const TeamPage = () => {
 
-   const [team,setTeam] = useState({matches:[]});
+  const [team, setTeam] = useState({ matches: [] });
 
-   const {teamName} = useParams();
+  const { teamName } = useParams();
 
-    useEffect(
-      () => {
+  useEffect(
+    () => {
 
-         const fetchMatches = async ()=> {
-            const response  = await fetch(`http://localhost:8080/teams/${teamName}`);
-            const data = await response.json();
+      const fetchMatches = async () => {
+        const response = await fetch(`http://localhost:8080/teams/${teamName}`);
+        const data = await response.json();
 
-             console.log(data);
-             setTeam(data);
-         };
+        console.log(data);
+        setTeam(data);
+      };
 
-         fetchMatches();
-      },[teamName]
+      fetchMatches();
+    }, [teamName]
 
-    );
+  );
 
 
-    if(!team || !team.teamName)
-    {
-      return <h1>Team not Found</h1>
-    }
+  if (!team || !team.teamName) {
+    return <h1>Team not Found</h1>
+  }
 
   return (
     <div className="TeamPage">
-          <h1>{team.teamName}</h1>
-          <MatchDetailCard teamName ={team.teamName} match={team.matches[0]}/>
-          
-          {
-          team.matches.slice(1).map(match=> <MatchSmallCard teamName ={team.teamName} match={match}/>)
-          
-          }
+      <div className="team-name-section">
+          <h1 className="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">
+      Wins / Losses
+      <PieChart
+    data={[
+    { title: 'Wins', value: team.totalWins, color: '#4da375' },
+    { title: 'Losses', value:team.totalMatches - team.totalWins, color: '#a34d5d' },
+       ]}
+     />;
+      </div>
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
+
+      {
+        team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} />)
+
+      }
+      <div className="more-link">
+        <a href="#">More ></a>
+      </div>
 
     </div>
   );
